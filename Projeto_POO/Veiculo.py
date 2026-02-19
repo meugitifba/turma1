ec27dcbee424753f61670448747e94fc50aa269b
andreborges22 <andreborges22@gmail.com>
2026-02-19 18:01:46 -0300

    Apagando as mensagens de retorno

--- Projeto_POO/Veiculo.py
+++ Projeto_POO/Veiculo.py
@@ -1,24 +1,25 @@
 class Veiculo:
     def dirigir(self):
-        return "O veículo está se movendo."
+        return ""
 
     def descricao(self):
         return f"{self.__class__.__name__}: {self.dirigir()}"
 
 class Carro(Veiculo):
     def dirigir(self):
-        return "O carro está acelerando na estrada."
+        return ""
 
 class Moto(Veiculo):
     def dirigir(self):
-        return "A moto está fazendo uma curva."
+        return ""
 
 class Caminhao(Veiculo):
     def dirigir(self):
-        return "O caminhão está transportando carga."
+        return ""
 
 # Programa principal
 veiculos = [Carro(), Moto(), Caminhao()]
 
 for v in veiculos:
+
     print(v.descricao())
